a4d05ec3aab23269f5d1992f9ec55453ad891d8f
YaraLit <yaroslav.litinenko@gmail.com>
2021-11-15 16:50:15 +0200

    Правки Ярослава на выходных 13 и 14 ноября

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import ajax_reg, entry, register, sign_out, login
+from .views import register, entry, sign_out, ajax_reg
 
 urlpatterns = [
-    path('register', register, name='register'),
-    path('login', login, name='login'),
+    path('register', register),
     path('entry', entry),
-    path('sign_out', sign_out),
+    path('logout', sign_out),
     path('ajax_reg', ajax_reg),
 ]
